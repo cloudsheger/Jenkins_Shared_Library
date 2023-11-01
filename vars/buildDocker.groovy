f7d568aa4ad75214e7a1147efd05e527b80de849
cloudsheger <cloudsheger@gmail.com>
2023-10-31 21:08:07 -0400

    updated jenkins.lib

--- vars/buildDocker.groovy
+++ vars/buildDocker.groovy
@@ -1,12 +1,3 @@
-def call() {
-    return {
-        stage('Build Docker Image') {
-            steps {
-                script {
-                    def customImageName = "cloudsheger/simple-java-app:${env.BUILD_NUMBER}"
-                    sh "docker build -t $customImageName ."
-                }
-            }
-        }
-    }
+def call(String customImageName) {
+    sh "docker build -t ${customImageName} ."         
 }

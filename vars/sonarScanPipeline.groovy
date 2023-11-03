5ce1ec18c759df95e6bd43c186497a1d314e1216
cloudsheger <cloudsheger@gmail.com>
2023-11-03 00:47:07 -0400

    updated par script

--- vars/sonarScanPipeline.groovy
+++ vars/sonarScanPipeline.groovy
@@ -1,13 +1,12 @@
 // vars/sonarScanPipeline.groovy
-import utils.parameters 
 // Import utility scripts from the src directory
 
 def call(Map params) {
-    def sonarParams = utils.parameters.getSonarParameters(params)
+    //def sonarParams = utils.parameters.getSonarParameters(params)
                     // Maven build and SonarQube scan command using sonarParams
                     sh "mvn clean verify sonar:sonar \
-                      -Dsonar.projectKey=${sonarParams.SONAR_PROJECT_KEY} \
-                      -Dsonar.projectName='${sonarParams.SONAR_PROJECT_NAME}' \
-                      -Dsonar.host.url=${sonarParams.SONAR_HOST_URL} \
-                      -Dsonar.token=${sonarParams.SONAR_TOKEN}"
+                      -Dsonar.projectKey=${params.SONAR_PROJECT_KEY} \
+                      -Dsonar.projectName='${params.SONAR_PROJECT_NAME}' \
+                      -Dsonar.host.url=${params.SONAR_HOST_URL} \
+                      -Dsonar.token=${params.SONAR_TOKEN}"
 }

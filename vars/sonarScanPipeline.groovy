107c700077eb57fe9cffec6a4a0040e8785742b1
cloudsheger <cloudsheger@gmail.com>
2023-11-07 23:45:58 -0500

    added test

--- vars/sonarScanPipeline.groovy
+++ vars/sonarScanPipeline.groovy
@@ -2,7 +2,7 @@ def call(Map params) {
     def projectKey = params.projectKey ?: 'shared-lib'
     def projectName = params.projectName ?: 'shared-lib'
     def sonarHostUrl = params.sonarHostUrl ?: 'http://localhost:9000'
-    //def sonarToken = params.sonarToken ?: 'deafult sonar token required'
+    def sonarToken = params.sonarToken ?: 'deafult sonar token required'
 
   echo "Running SonarQube scan for project: ${projectName}"
 

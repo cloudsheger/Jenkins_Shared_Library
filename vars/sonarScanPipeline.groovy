98945456c4a306738ce2da9f4b9f96f81a1feb21
cloudsheger <cloudsheger@gmail.com>
2023-11-08 00:20:02 -0500

    added test

--- vars/sonarScanPipeline.groovy
+++ vars/sonarScanPipeline.groovy
@@ -2,20 +2,16 @@ def call(Map params) {
     def projectKey = params.projectKey ?: 'shared-lib'
     def projectName = params.projectName ?: 'shared-lib'
     def sonarHostUrl = params.sonarHostUrl ?: 'http://localhost:9000'
-   // def sonarToken = params.sonarToken ?: 'deafult sonar token required'
+    def sonarToken = params.sonarToken ?: 'deafult sonar token required'
 
   echo "Running SonarQube scan for project: ${projectName}"
 
-withCredentials([string(credentialsId: 'SONAR_TOKEN_ID', variable: 'SONAR_TOKEN')]) {
-        withSonarQubeEnv('SonarScanner') {
-            sh """
-                mvn sonar:sonar \
-                -Dsonar.projectKey='${projectKey}' \
-                -Dsonar.projectName='${projectName}' \
-                -Dsonar.host.url='${sonarHostUrl}' \
-                -Dsonar.login='\${SONAR_TOKEN}'
-            """
-        }
+  withSonarQubeEnv('SonarScanner') {
+    sh "mvn sonar:sonar \
+       -Dsonar.projectKey=${projectKey} \
+       -Dsonar.projectName='${projectName}' \
+       -Dsonar.host.url=${sonarHostUrl} \
+       -Dsonar.login=${sonarToken}"
     }
 }
 

cd10f3951578606ffdee6fc72f1121368054424c
cloudsheger <cloudsheger@gmail.com>
2023-11-07 22:01:53 -0500

    added test

--- vars/sonarScanPipeline.groovy
+++ vars/sonarScanPipeline.groovy
@@ -4,11 +4,14 @@ def call(Map params) {
     def sonarHostUrl = params.sonarHostUrl ?: 'http://localhost:9000'
     def sonarToken = params.sonarToken ?: 'deafult sonar token required'
 
-    echo "Running SonarQube scan for project: ${projectName}"
-    sh "mvn clean verify sonar:sonar \
+  echo "Running SonarQube scan for project: ${projectName}"
+  
+  withSonarQubeEnv('SonarScanner') {
+    sh "mvn sonar:sonar \
        -Dsonar.projectKey=${projectKey} \
        -Dsonar.projectName='${projectName}' \
        -Dsonar.host.url=${sonarHostUrl} \
        -Dsonar.login=${sonarToken}"
+    }
 }
 

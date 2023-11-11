0f69f4b13ebb7b6dbfee330310dc0cf7a5427ed4
cloudsheger <cloudsheger@gmail.com>
2023-11-10 22:24:40 -0500

    variable credentialsId fixed

--- vars/pushToArtifactory.groovy
+++ vars/pushToArtifactory.groovy
@@ -4,10 +4,10 @@ def call(Map config) {
     def dockerImage = "${config.DOCKER_REGISTRY}/${config.DOCKER_REPO}/${config.IMAGE_NAME}:${config.BUILD_NUMBER}"
     
     // Get Artifactory credentials
-    def credentials-id = credentials(config.ARTIFACTORY_CREDENTIALS_ID)
+    def credentialsId = credentials(config.ARTIFACTORY_CREDENTIALS_ID)
 
     // Push Docker image to Artifactory using docker.withRegistry
-    docker.withRegistry("https://${config.DOCKER_REGISTRY}", 'credentials-id') {
+    docker.withRegistry("https://${config.DOCKER_REGISTRY}", credentialsId) {
         sh "docker push ${dockerImage}"
     }
 }

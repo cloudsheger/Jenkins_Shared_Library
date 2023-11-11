ed23f37c370a0af39aa288d432519e79aea5d2c7
cloudsheger <cloudsheger@gmail.com>
2023-11-10 20:21:50 -0500

    updated jenkinsfile

--- vars/pushToArtifactory.groovy
+++ vars/pushToArtifactory.groovy
@@ -1,36 +1,10 @@
 // vars/pushToArtifactory.groovy
 
 def call(Map config) {
-    pipeline {
-        agent any
-
-        stages {
-            stage('Push to Artifactory') {
-                steps {
-                    script {
-                        pushToArtifactory(config)
-                    }
-                }
-            }
-        }
-
-        post {
-            success {
-                echo 'Docker image pushed to Artifactory successfully!'
-            }
-            failure {
-                echo 'Docker image push to Artifactory failed!'
-            }
-        }
-    }
-}
-
-def pushToArtifactory(Map config) {
     def dockerImage = "${config.DOCKER_REGISTRY}/${config.DOCKER_REPO}/${config.IMAGE_NAME}:${config.BUILD_NUMBER}"
-
     // Log in to Artifactory Docker registry
     withCredentials([usernamePassword(credentialsId: config.ARTIFACTORY_CREDENTIALS_ID, usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
-        sh "docker login -u ${USERNAME} -p ${PASSWORD} ${config.DOCKER_REGISTRY}"
+        sh "docker login -u ${USERNAME} -p ${PASSWORD} https://${config.DOCKER_REGISTRY}"
     }
 
     // Push Docker image to Artifactory

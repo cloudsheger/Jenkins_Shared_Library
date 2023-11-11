ec685cb406f044ab7329545cedf1828f8c8b96bc
cloudsheger <cloudsheger@gmail.com>
2023-11-11 01:05:08 -0500

    shared library updated

--- vars/pushToArtifactory.groovy
+++ vars/pushToArtifactory.groovy
@@ -2,12 +2,11 @@
 
 def call(Map config) {
     def dockerImage = "${config.DOCKER_REGISTRY}/${config.DOCKER_REPO}/${config.IMAGE_NAME}:${config.BUILD_NUMBER}"
-    
-    // Get Artifactory credentials
-    def credentialsId = credentials(config.ARTIFACTORY_CREDENTIALS_ID)
 
     // Push Docker image to Artifactory using docker.withRegistry
-    docker.withRegistry("https://${config.DOCKER_REGISTRY}", 'credentialsId') {
+    //// Get Artifactory credentials
+    //def credentialsId = credentials(config.ARTIFACTORY_CREDENTIALS_ID)
+    docker.withRegistry("https://${config.DOCKER_REGISTRY}", config.ARTIFACTORY_CREDENTIALS_ID) {
         sh "docker push ${dockerImage}"
     }
 }

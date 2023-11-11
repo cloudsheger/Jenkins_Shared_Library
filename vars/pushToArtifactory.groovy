dc38a36dcfbc56123f3869b3d799e67082935f91
cloudsheger <cloudsheger@gmail.com>
2023-11-10 20:33:36 -0500

    updated jenkinsfile

--- vars/pushToArtifactory.groovy
+++ vars/pushToArtifactory.groovy
@@ -4,10 +4,10 @@ def call(Map config) {
     def dockerImage = "${config.DOCKER_REGISTRY}/${config.DOCKER_REPO}/${config.IMAGE_NAME}:${config.BUILD_NUMBER}"
     
     // Get Artifactory credentials
-    def credentials = credentials(config.ARTIFACTORY_CREDENTIALS_ID)
+    def credentials-id = credentials(config.ARTIFACTORY_CREDENTIALS_ID)
 
     // Push Docker image to Artifactory using docker.withRegistry
-    docker.withRegistry("https://${config.DOCKER_REGISTRY}", credentials.id) {
+    docker.withRegistry("https://${config.DOCKER_REGISTRY}", credentials-id) {
         sh "docker push ${dockerImage}"
     }
 }

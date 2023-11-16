819ccea09dc95449112bfdf6574f94d56a649db4
cloudsheger <cloudsheger@gmail.com>
2023-11-15 21:00:31 -0500

    updated shared lib

--- vars/pushToArtifactory.groovy
+++ vars/pushToArtifactory.groovy
@@ -1,7 +1,7 @@
 // vars/pushToArtifactory.groovy
 
-def call(Map config) {
-    def dockerImage = "${config.DOCKER_REGISTRY}/${config.DOCKER_REPO}/${config.IMAGE_NAME}:${config.BUILD_NUMBER}"
+def buildAndPushImage(Map config, String tagName) {
+    def dockerImage = "${config.DOCKER_REGISTRY}/${config.DOCKER_REPO}/${config.IMAGE_NAME}:${tagName}"
 
     // Pull Docker image from Artifactory using docker.withRegistry
     docker.withRegistry("https://${config.DOCKER_REGISTRY}", config.ARTIFACTORY_CREDENTIALS_ID) {
@@ -9,10 +9,15 @@ def call(Map config) {
     }
 
     // Build Docker image using cache
-    sh "docker build --cache-from=${dockerImage} -t ${config.IMAGE_NAME}:${config.BUILD_NUMBER} ."
+    sh "docker build --cache-from=${dockerImage} -t ${config.IMAGE_NAME}:${tagName} ."
 
     // Push Docker image to Artifactory using docker.withRegistry
     docker.withRegistry("https://${config.DOCKER_REGISTRY}", config.ARTIFACTORY_CREDENTIALS_ID) {
         sh "docker push ${dockerImage}"
     }
+}
+
+def call(Map config) {
+    buildAndPushImage(config, 'latest')
+    buildAndPushImage(config, config.BUILD_NUMBER)
 }

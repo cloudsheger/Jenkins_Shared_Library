c8aa449e1ae9b697eb041f5241506ba1640d3537
Jemal <117579792+cloudsheger@users.noreply.github.com>
2023-11-23 19:48:18 -0500

    Update buildAndPushDockerImage.groovy

--- vars/buildAndPushDockerImage.groovy
+++ vars/buildAndPushDockerImage.groovy
@@ -5,10 +5,10 @@ def call(Map config) {
 
     echo "Building Docker image..."
 
-    // Use Artifactory API key credentials
-    withCredentials([string(credentialsId: config.ARTIFACTORY_API_KEY_CREDENTIALS_ID, variable: "artifactoryApiToken")]) {
+    // Use Docker Pipeline plugin to authenticate with Docker registry
+    docker.withRegistry(config.DOCKER_REGISTRY, config.DOCKER_CREDS_ID) {
         sh "docker build -f docker/Dockerfile -t ${imageName} ."
-        sh "echo ${artifactoryApiToken} | docker login -u 'jems' --password-stdin ${config.DOCKER_REGISTRY}"
         sh "docker push ${imageName}"
     }
 }
+
